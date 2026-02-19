a2bbc0cf50e402c30ea746cd72457b8ede77216d
colemutter <cole.mutter@icloud.com>
2026-02-19 12:44:34 -0700

    added junit lib and empty testclass

---
+++ src/test/TestMovingPieces.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.Test;
+
+public class TestMovingPieces {
+    @Test
+    public void testRockThrowerMovement() {
+        System.out.println("testMovingPieces");
+    }
+
+}
